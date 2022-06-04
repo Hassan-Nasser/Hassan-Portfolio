b0876af4a27d78dd95af13fcda9672010eae91b3
aya-abdullah <ayalaban888@yahoo.com>
2022-06-05 01:32:54 +0200

    ADD multi carousel for projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,12 +1,25 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import "./Portfolio.scss";
 import Modal from "react-bootstrap/Modal";
 import { Scrollbars } from "react-custom-scrollbars";
 import projects from "../../constants/projects.json";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-// import PerfectScrollbar from 'react-perfect-scrollbar';
-// import 'react-perfect-scrollbar/dist/css/styles.css';
-
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 class Portfolio extends Component {
   constructor(props) {
     super(props);
@@ -15,9 +28,12 @@ class Portfolio extends Component {
       header: "",
       link: "",
       description: "",
+      additionalTransfrom: 0
     };
   }
-  
+
+
+
   setShow = (flag, header = "", link = "", description = "") => {
     this.setState({ show: flag });
     if (flag) {
@@ -25,7 +41,67 @@ class Portfolio extends Component {
     }
   };
 
+  CustomRightArrow = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      carouselState: { currentSlide, deviceType }
+    } = rest;
+    // onMove means if dragging or swiping in progress.
+    return <button onClick={() => onClick()} />;
+  };
+
+
   render() {
+    const CustomSlider = ({ carouselState }) => {
+      let value = 0;
+      let carouselItemWidth = 0;
+      if (this.Carousel) {
+        carouselItemWidth = this.Carousel.state.itemWidth;
+        const maxTranslateX = Math.round(
+          // so that we don't over-slide
+          carouselItemWidth *
+          (this.Carousel.state.totalItems -
+            this.Carousel.state.slidesToShow) +
+          150
+        );
+        value = maxTranslateX / 100; // calculate the unit of transform for the slider
+      }
+      const { transform } = carouselState;
+      return (
+        <div className="custom-slider">
+          <input
+            type="range"
+            value={Math.round(Math.abs(transform) / value)}
+            defaultValue={0}
+            max={
+              (carouselItemWidth *
+                (carouselState.totalItems - carouselState.slidesToShow) +
+                (this.state.additionalTransfrom === 150 ? 0 : 150)) /
+              value
+            }
+            onChange={e => {
+              if (this.Carousel.isAnimationAllowed) {
+                this.Carousel.isAnimationAllowed = false;
+              }
+              const nextTransform = e.target.value * value;
+              const nextSlide = Math.round(nextTransform / carouselItemWidth);
+              if (
+                e.target.value == 0 &&
+                this.state.additionalTransfrom === 150
+              ) {
+                this.Carousel.isAnimationAllowed = true;
+                this.setState({ additionalTransfrom: 0 });
+              }
+              this.Carousel.setState({
+                transform: -nextTransform, // padding 20px and 5 items.
+                currentSlide: nextSlide
+              });
+            }}
+            className="custom-slider__input"
+          />
+        </div>
+      );
+    };
     return (
       <>
         <div className="container">
@@ -37,29 +113,49 @@ class Portfolio extends Component {
             </p>
           </header>
           <div className="row">
-           {projects.map((project)=>
-            <div key={project.id} className="col-lg-4 col-md-6 col-sm-12">
-            <article className="box style2">
-              <button
-                className="transparent image featured"
-                onClick={() =>
-                  this.setShow(
-                    true,
-                    project.name,
-                    project.url,
-                    project.description
-                  )
+            <Carousel
+              ssr={false}
+              infinite
+              ref={el => (this.Carousel = el)}
+              partialVisbile={false}
+              customButtonGroup={<CustomSlider />}
+              itemClass="slider-image-item"
+              responsive={responsive}
+              containerClass="carousel-container-with-scrollbar"
+              additionalTransfrom={-this.state.additionalTransfrom}
+            
+              beforeChange={nextSlide => {
+                if (nextSlide !== 0 && this.state.additionalTransfrom !== 150) {
+                  this.setState({ additionalTransfrom: 150 });
                 }
-              >
-                <img src={require(`../../images/${project.cover}`)} alt="" />
-                <h3>{project.name}</h3>
-              </button>
-              {/* <!-- <button data-toggle="modal" data-target="#exampleModal" type="button" class="btn btn-primary"><img src="images/pic01.jpg" alt="" /></button> --> */}
-            </article>
-          </div>
-           )}
-           
-         
+                if (nextSlide === 0 && this.state.additionalTransfrom === 150) {
+                  this.setState({ additionalTransfrom: 0 });
+                }
+              }}
+            >
+              {projects && projects.map((project) =>
+                <div key={project.id} className="col-lg-4 col-md-6 col-sm-12" class="image-container increase-size">
+                  <article className="box style2" draggable={false}
+                    style={{ width: "100%", cursor: "pointer" }}>
+                    <button
+                      className="transparent image featured"
+                      onClick={() =>
+                        this.setShow(
+                          true,
+                          project.name,
+                          project.url,
+                          project.description
+                        )
+                      }
+                    >
+                      <img src={require(`../../images/${project.cover}`)} alt="" />
+                      <h3>{project.name}</h3>
+                    </button>
+                    {/* <!-- <button data-toggle="modal" data-target="#exampleModal" type="button" class="btn btn-primary"><img src="images/pic01.jpg" alt="" /></button> --> */}
+                  </article>
+                </div>
+              )}
+            </Carousel>
           </div>
           <footer>
             <a href="#contact" className="button large scrolly">
